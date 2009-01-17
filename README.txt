5551ff6a7e626c796bc60b361fcfc2a5f525341a
Peter Bengtsson <peter@fry-it.com>
2009-01-17 17:57:31 +0000

    Added .mo

---
+++ README.txt
@@ -0,0 +1,10 @@
+I18N
+====
+
+Make messages:
+
+ django-admin.py makemessages -l sv-SE
+ 
+Compile messages:
+
+ django-admin.py compilemessages
--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'sv-SE'
+LANGUAGE_CODE = 'sv'
 
 SITE_ID = 1
 
