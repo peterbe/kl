dfcafd934a858eb90b6b517e9b075ee8f448c3ce
Peter Bengtsson <peter@fry-it.com>
2009-05-05 10:47:46 +0100

    Sparklines to not include today's searches

--- search/googlecharts.py
+++ search/googlecharts.py
@@ -6,16 +6,18 @@ from pygooglechart import SparkLineChart
 
 # app
 from models import Search
+from utils import print_sql
 
 def get_sparklines(width, height, background_color='efefef'):
     """wrap _get_sparklines() but first work out the data"""
     data = defaultdict(int)
     
     today = datetime.datetime.today()
+    today_date = datetime.datetime(today.year, today.month, today.day)
     first_date = datetime.datetime(today.year, today.month, 1)
     searches = Search.objects
     searches = searches.filter(add_date__gte=first_date,
-                               add_date__lt=today)
+                               add_date__lt=today_date)
     for search in searches:
         data[search.add_date.day] += 1
     
