5c9584887fddc6ee9671553f949222208a1653b3
Peter Bengtsson <peter@fry-it.com>
2010-05-20 19:26:31 +0100

    modified search/management/commands/lookup_definitions.py

--- search/management/commands/lookup_definitions.py
+++ search/management/commands/lookup_definitions.py
@@ -103,6 +103,8 @@ class Command(BaseCommand):
                     definition = _get_word_definition_scrape(word, language=lang)
                     
                 if definition:
+                    if len(definition) > 250:
+                        definition = definition[:250-3]+'...'
                     add_word_definition(word, definition, language=lang)
                     print "FOUND!", repr(word)
                     count_success += 1
