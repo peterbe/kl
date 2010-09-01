02047054404fb6605e9b048a515c8f66f632bdfe
Peter Bengtsson <peter@fry-it.com>
2010-09-01 08:37:29 -0400

    modified search/management/commands/add_new_word.py

--- search/management/commands/add_new_word.py
+++ search/management/commands/add_new_word.py
@@ -1,3 +1,4 @@
+import re
 import os
 import glob
 from django.conf import settings
@@ -13,9 +14,8 @@ class Command(NoArgsCommand):
         from search.views import ALL_LANGUAGE_OPTIONS, _get_word_definition, \
           _get_word_definition_scrape, add_word_definition
         
-        word = unicode(raw_input("Word: ").strip(), 'utf8')
-        length = len(word)
-        print repr(word), length
+        words = unicode(raw_input("Word(s): ").strip(), 'utf8')
+        words = re.split(',?\s*|\s', words)
         
         
         langs = []
@@ -23,27 +23,31 @@ class Command(NoArgsCommand):
             answer = raw_input('\t%s [y/N] ' % option['label'].encode('latin1'))
             if answer.lower() in ('y','yes'):
                 langs.append(option['code'].lower())
+
                 
         # insert them finally!
         from search.models import Word
         
         definition = ''
-        
-        for lang in langs:
-            word_object = Word.objects.create(word=word, language=lang, length=length)
-            cache_key = '_find_alternatives_%s_%s' % (word, lang)
-            cache.delete(cache_key)
+        for word in words:
+            length = len(word)
+            print repr(word), length
             
-            if not definition:
-                try:
-                    definition = _get_word_definition(word, language=lang)
-                except AttributeError:
-                    # sometimes you get a weird AttributeError in nltk
-                    pass
+            for lang in langs:
+                word_object = Word.objects.create(word=word, language=lang, length=length)
+                cache_key = '_find_alternatives_%s_%s' % (word, lang)
+                cache.delete(cache_key)
                 
                 if not definition:
-                    _get_word_definition_scrape(word, language=lang)
-                
-            if definition:
-                add_word_definition(word, definition, language=lang)
-
+                    try:
+                        definition = _get_word_definition(word, language=lang)
+                    except AttributeError:
+                        # sometimes you get a weird AttributeError in nltk
+                        pass
+                    
+                    if not definition:
+                        _get_word_definition_scrape(word, language=lang)
+                    
+                if definition:
+                    add_word_definition(word, definition, language=lang)
+    
