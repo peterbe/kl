d27011847972cb0631ca38f34897998efc097075
Peter Bengtsson <peter@fry-it.com>
2011-01-12 20:26:06 +0000

    ad depedent of host

--- context_processors.py
+++ context_processors.py
@@ -17,7 +17,7 @@ from search.googlecharts import get_sparklines_cached
 from search.forms import QUIZZES
 
 def context(request):
-    
+
     data = {'TEMPLATE_DEBUG': settings.TEMPLATE_DEBUG,
             'DEBUG': settings.DEBUG,
             'HOME': settings.HOME,
@@ -28,20 +28,20 @@ def context(request):
             'GOOGLE_ANALYTICS': settings.GOOGLE_ANALYTICS,
             'GIT_REVISION_DATE': settings.GIT_REVISION_DATE,
             }
-    
+
     data['quiz_question'] = random.choice(QUIZZES.keys())
-    
+
     #if settings.DEBUG:
     #    data['ADMIN_MEDIA_PREFIX'] = '/'
     #else:
     #    data['ADMIN_MEDIA_PREFIX'] = settings.ADMIN_MEDIA_PREFIX
-         
+
     #http_user_agent = request.META.get('HTTP_USER_AGENT', None)
-    
+
     #request.mobile_version = False
     #request.iphone_version = False
-    
-    
+
+
     if request.iphone:
         data['iphone_version'] = True
     if request.mobile:
@@ -51,13 +51,13 @@ def context(request):
 
     language = request.LANGUAGE_CODE
     data.update(get_search_stats(language))
-    
+
     data.update(get_saved_cookies(request))
-    
+
     data.update(dict(language_options=get_language_options(request)))
-    
+
     data['render_form_ts'] = int(time.time())
-    
+
     # for the link the Searches summary of the latest month
     data['searches_summary_link'] = datetime.datetime.today().strftime('/searches/%Y/%B/')
 
@@ -65,38 +65,39 @@ def context(request):
         sparklines_url = get_sparklines_cached(150, 100, only_if_cached=True)
         if sparklines_url:
             data['sparklines_url'] = sparklines_url
-            
+
             today = datetime.datetime.today()
             first_date = datetime.datetime(today.year, today.month, 1)
             data['sparklines_href'] = '/statistics/graph/?daterange='+\
                                     quote(first_date.strftime('%Y/%m/%d')) +\
                                     quote(' - ') +\
                                     quote(today.strftime('%Y/%m/%d'))
-                
+
         else:
             data['sparklines_script'] = '<script type="text/javascript">'\
              'var SPARKLINES=true;</script>'
-    
+
     current_url = request.build_absolute_uri()
-    
+
     canonical_url = get_canonical_url(current_url)
     if canonical_url:
         data['canonical_url'] = canonical_url
-    
+
     if request.META.get('GEO'):
         data['geo'] = request.META.get('GEO')
-        
+
         if not settings.DEBUG:
             if request.session.get('has_searched'):
                 data['amazon_advert'] = get_amazon_advert(data['geo'])
-    
-        
+
+
     data['use_google_analytics'] = data.get('use_google_analytics',
                                             not settings.DEBUG)
 
     data['show_crossing_the_world_link'] = '/crossing-the-world' not in current_url
-    
+
     data['this_django_instance'] = os.path.basename(os.path.dirname(__file__))
-    
-    
+
+    data['show_publishermedia_ad'] = request.get_host() == 'crosstips.org'
+
     return data
